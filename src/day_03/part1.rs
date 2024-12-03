82dfd89b0e1a5114cc46db39e804f674d5fd572b
Scriptor25 <f.schreiber.2006@proton.me>
2024-12-03 09:20:37 +0100

    solved day 3

--- src/day_03/part1.rs
+++ src/day_03/part1.rs
@@ -1,3 +1,17 @@
+use regex::Regex;
+
 pub fn part1(input: String) -> i32 {
-    0
+    let reg = Regex::new(r"mul\((0|([1-9][0-9]*)),(0|([1-9][0-9]*))\)").unwrap();
+
+    let mut sum = 0;
+
+    for cap in reg.captures_iter(input.as_str()) {
+        let x_str = &cap[1];
+        let y_str = &cap[3];
+        let x: i32 = x_str.parse().expect("failed to parse x");
+        let y: i32 = y_str.parse().expect("failed to parse y");
+        sum += x * y;
+    }
+
+    sum
 }
--- src/day_03/part2.rs
+++ src/day_03/part2.rs
@@ -1,3 +1,30 @@
+use regex::Regex;
+
 pub fn part2(input: String) -> i32 {
-    0
+    let reg =
+        Regex::new(r"(mul\((0|([1-9][0-9]*)),(0|([1-9][0-9]*))\))|(do\(\))|(don't\(\))").unwrap();
+
+    let mut sum = 0;
+    let mut do_mul = true;
+
+    for cap in reg.captures_iter(input.as_str()) {
+        let c = &cap[0];
+        if c.starts_with("mul") {
+            if do_mul {
+                let x_str = &cap[2];
+                let y_str = &cap[4];
+                let x: i32 = x_str.parse().expect("failed to parse x");
+                let y: i32 = y_str.parse().expect("failed to parse y");
+                sum += x * y;
+            }
+        } else if c.starts_with("don't") {
+            do_mul = false;
+        } else if c.starts_with("do") {
+            do_mul = true;
+        } else {
+            panic!("unhandled match {c}");
+        }
+    }
+
+    sum
 }

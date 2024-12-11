f56cfbbde00cb27d8f72ae0bf019cd4c210b45a6
Scriptor25 <f.schreiber.2006@proton.me>
2024-12-11 10:08:32 +0100

    solved day 9

--- src/day_09/part1.rs
+++ src/day_09/part1.rs
@@ -1,3 +1,51 @@
+use std::collections::VecDeque;
+
 pub fn part1(input: String) -> usize {
-    0
+    let in_chars = input.chars().collect::<Vec<char>>();
+    let mut file_indices = Vec::new();
+    let mut idx = 0;
+
+    let mut free_indices = VecDeque::new();
+
+    for i in 0..in_chars.len() {
+        let ic = in_chars[i] as i32;
+        if i % 2 == 0 {
+            let used = ic - 0x30;
+            for _ in 0..used {
+                file_indices.push(idx);
+            }
+            idx += 1;
+        } else {
+            let free = ic - 0x30;
+            for _ in 0..free {
+                free_indices.push_back(file_indices.len());
+                file_indices.push(!0);
+            }
+        }
+    }
+
+    let mut end = 0usize;
+    for i in (0..file_indices.len()).rev() {
+        if free_indices.is_empty() {
+            end = i + 1;
+            break;
+        }
+
+        if file_indices[i] == !0 {
+            free_indices.pop_back();
+        } else {
+            let free_idx = free_indices.pop_front().unwrap();
+            file_indices.swap(free_idx, i);
+        }
+    }
+
+    file_indices.drain(end..file_indices.len());
+
+    let mut checksum = 0usize;
+    for i in 0..file_indices.len() {
+        let oc = file_indices[i];
+        checksum += i * oc;
+    }
+
+    checksum
 }
--- src/day_09/part2.rs
+++ src/day_09/part2.rs
@@ -1,3 +1,79 @@
+use std::collections::VecDeque;
+
+fn print_blocks(blocks: &Vec<(usize, usize)>) {
+    blocks.iter().for_each(|&(block_idx, block_size)| {
+        for i in 0..block_size {
+            if block_idx == !0 {
+                print!(".");
+            } else {
+                print!("{}", block_idx);
+            }
+        }
+    });
+    println!();
+}
+
 pub fn part2(input: String) -> usize {
-    0
+    let in_chars = input.chars().collect::<Vec<char>>();
+
+    let mut file_blocks = Vec::new();
+
+    let mut idx = 0usize;
+    for i in 0..in_chars.len() {
+        let ic = in_chars[i] as i32;
+        let size = (ic - 0x30) as usize;
+        if size == 0 {
+            continue;
+        }
+        if i % 2 == 0 {
+            file_blocks.push((idx, size));
+            idx += 1;
+        } else {
+            file_blocks.push((!0, size));
+        }
+    }
+
+    {
+        let mut i = file_blocks.len();
+        while i > 0 {
+            i -= 1;
+
+            let (file_idx, file_size) = file_blocks[i];
+            if file_idx == !0 {
+                continue;
+            }
+
+            for j in 0..i {
+                let (free_idx, free_size) = file_blocks[j];
+                if free_idx != !0 {
+                    continue;
+                }
+                if free_size == file_size {
+                    file_blocks.swap(i, j);
+                    break;
+                }
+                if free_size > file_size {
+                    file_blocks[j].1 = free_size - file_size;
+                    file_blocks[i] = (!0, file_size);
+                    file_blocks.insert(j, (file_idx, file_size));
+                    i += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    let mut checksum = 0usize;
+    let mut idx = 0usize;
+    for i in 0..file_blocks.len() {
+        let (file_idx, file_size) = file_blocks[i];
+        if file_idx != !0 {
+            for j in 0..file_size {
+                checksum += (idx + j) * file_idx;
+            }
+        }
+        idx += file_size;
+    }
+
+    checksum
 }

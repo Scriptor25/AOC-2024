3973a8dba08cc758a9de65a371c0abd3bd999d17
Scriptor25 <f.schreiber.2006@proton.me>
2024-12-20 17:20:51 +0100

    began day 12

--- src/day_12/part1.rs
+++ src/day_12/part1.rs
@@ -1,3 +1,28 @@
-pub fn part1(_input: String) -> usize {
-    0
+fn find_cells(dst: &mut Vec<(usize, usize)>, map: &mut Vec<Vec<char>>, i: usize, j: usize) {
+    
+}
+
+pub fn part1(input: String) -> usize {
+    let mut map = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+
+    let mut sizes: Vec<(usize, usize)> = Vec::new();
+
+    for j in 0..map.len() {
+        for i in 0..map[0].len() {
+            if map[j][i] == '#' {
+                continue;
+            }
+            let mut cells = Vec::new();
+            find_cells(&mut cells, &mut map, i, j);
+        }
+    }
+
+    let mut sum = 0;
+    for size in sizes {
+        sum += size.0 * size.1
+    }
+    sum
 }
